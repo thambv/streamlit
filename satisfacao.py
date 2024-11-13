3fdf606a5372b283ba9c34cb93657d10ff5028c2
thambv <94761355+thambv@users.noreply.github.com>
2024-11-13 11:40:55 -0300

    Update satisfacao.py

--- satisfacao.py
+++ satisfacao.py
@@ -1,15 +1,7 @@
 import streamlit as st
-import plotly.express as px
-import pandas as pd
 
 st.write('Vamos aprender streamlit juntos!')
 st.title('Nível de Satisfação do Cliente')
-st.header('Este é o subtítulo')
-st.subheader('Este é o terceiro subtítulo')
-st.markdown('Este é o texto')
-st.caption('Esta é a legenda')
-st.code('x=2021')
-st.latex(r''' a+a r^1+a r^2+a r^3 ''')
 
 st.slider('Qual é o seu nível de satisfação?',  min_value=0, max_value=100)
 
