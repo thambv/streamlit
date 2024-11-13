b3c7af6b9b8d312900d89bdf016b59c11fca621e
thambv <94761355+thambv@users.noreply.github.com>
2024-11-13 11:20:15 -0300

    Create teste2.py

---
+++ teste2.py
@@ -0,0 +1,13 @@
+!pip install streamlit
+import streamlit as st
+import plotly.express as px
+import pandas as pd
+
+st.write('Vamos aprender streamlit juntos!')
+st.title('Este é o título do app')
+st.header('Este é o subtítulo')
+st.subheader('Este é o terceiro subtítulo')
+st.markdown('Este é o texto')
+st.caption('Esta é a legenda')
+st.code('x=2021')
+st.latex(r''' a+a r^1+a r^2+a r^3 ''')
