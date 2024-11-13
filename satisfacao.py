02c62911939b16868a5957f04ca1dd9d6ec53af6
thambv <94761355+thambv@users.noreply.github.com>
2024-11-13 11:40:17 -0300

    Update satisfacao.py

--- satisfacao.py
+++ satisfacao.py
@@ -11,8 +11,5 @@ st.caption('Esta é a legenda')
 st.code('x=2021')
 st.latex(r''' a+a r^1+a r^2+a r^3 ''')
 
-nivel_satisfacao = st.select_slider(
-    'Qual é o seu nível de satisfação?',  # Texto para o usuário
-    options=range(0, 101, 10),  # Opções de 0 a 100 com intervalo de 10
-    value=50  # Valor inicial (por exemplo, 50%)
-)
+st.slider('Qual é o seu nível de satisfação?',  min_value=0, max_value=100)
+
